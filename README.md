a58959670a3bc779d2c4c145ca222a6f706c6f55
Okan <okso2004@gmail.com>
2021-12-29 14:07:03 +0100

    Finished README.md

---
+++ README.md
@@ -0,0 +1,21 @@
+# Password Generator
+
+To run this program, direct to the directory where the `main.py` file is stored and run it using the command `python main.py`
+
+You will be asked to enter the length of the password which should be generated.
+
+```
+Enter the length of the password 
+```
+
+Let's say our password should be eight characters long.
+
+```
+Enter the length of the password 8
+```
+
+Then the program will generate a password with eight random characters.
+
+```
+Your password is jGX§2"TO
+```
---
+++ main.py
@@ -0,0 +1,22 @@
+import string, random, os
+
+clear = lambda: os.system('clear') if os.name == "posix" else os.system("cls")
+
+string.ascii_letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!"§$%&/()=?' 
+
+length = int(input("Enter the length of the password ")) 
+
+password = "" 
+
+while length > 0: 
+    password += random.choice(string.ascii_letters) 
+    length -= 1 
+    
+    
+def main():
+    print(f"Your password is {password}")
+
+
+if __name__ == "__main__":
+    clear()
+    main()

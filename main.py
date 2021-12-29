6b6648aa0900fbf18b292abc9a9551d31622aafd
Okan <okso2004@gmail.com>
2021-12-29 14:08:27 +0100

    Polished main.py

--- main.py
+++ main.py
@@ -4,16 +4,17 @@ clear = lambda: os.system('clear') if os.name == "posix" else os.system("cls")
 
 string.ascii_letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!"§$%&/()=?' 
 
-length = int(input("Enter the length of the password ")) 
-
-password = "" 
-
-while length > 0: 
-    password += random.choice(string.ascii_letters) 
-    length -= 1 
     
     
 def main():
+
+    length = int(input("Enter the length of the password ")) 
+
+    password = "" 
+
+    while length > 0: 
+        password += random.choice(string.ascii_letters) 
+        length -= 1 
     print(f"Your password is {password}")
 
 
